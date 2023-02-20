8178b32a2f69a942e0928368cc57b55137ccb049
YaRo <roman.yatsenko@hneu.net>
2023-02-20 15:28:49 +0200

    Add greet page

--- hello/views.py
+++ hello/views.py
@@ -1,3 +1,22 @@
+import datetime
+
+
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return render(request, 'hello/index.html')
+
+def vlad(request):
+    return HttpResponse('Hello, Vlad!')
+
+def artem(request):
+    return HttpResponse('Hello, Artem!')
+
+def greet(request, name):
+    return render(request, 'hello/greet.html', context={
+        'name': name.capitalize(),
+        'time': datetime.datetime.now(),
+    })
