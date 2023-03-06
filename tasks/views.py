c7542c3c359696f718eafa772c0a4cb53bd222c4
YaRo <roman.yatsenko@hneu.net>
2023-03-06 14:30:35 +0200

    Add form Post Processing

--- tasks/views.py
+++ tasks/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
 
 from .forms import NewTaskForm
 
@@ -12,6 +13,13 @@ def index(request):
     })
 
 def add(request):
+    if request.method == 'POST':
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data['task']
+            tasks.append(task)
+            return redirect(reverse('index'))
+
     return render(request, 'tasks/add.html', context={
         'form': NewTaskForm(),
     })
